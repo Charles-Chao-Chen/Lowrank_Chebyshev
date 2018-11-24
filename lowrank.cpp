bf8e10d4f11b7f1e1d6640bb46e6e13db38b518a
Chao Chen <cchen10@stanford.edu>
2018-11-24 00:41:06 -0800

    added 2D scheme

--- lowrank.cpp
+++ lowrank.cpp
@@ -10,6 +10,10 @@ typedef Eigen::VectorXd Vector;
 class Kernel {
 public:
   Kernel() {};
+  double operator() (const Vector &x, const Vector &y) const {
+    //return (x-y).squaredNorm();
+    return 1.0/((x-y).squaredNorm());
+  }
   double operator() (double x, double y) const {
     return 1.0/((x-y)*(x-y));
   }
@@ -51,7 +55,7 @@ private:
   Vector Cheb;
 };
 
-void Compute_lowrank
+void Compute_lowrank_1d
 (const Kernel &K, 
 const Vector &X, double xmin, double xmax, 
 const Vector &Y, double ymin, double ymax,
@@ -68,37 +72,84 @@ const int r, Matrix &U, Matrix &S, Matrix &V) {
       S(i, j) = K( chebX(i), chebY(j) );
 }
 
+void Compute_lowrank
+(const Kernel &K,
+const Matrix &X, double xmin, double xmax,
+const Matrix &Y, double ymin, double ymax,
+const int r, Matrix &U, Matrix &S, Matrix &V) {
+
+  int n = X.rows();
+  Matrix Ux(n, r), Uy(n, r), Vx(n, r), Vy(n, r);
+  Interpolation Ix(xmin, xmax, r);
+  Interpolation Iy(ymin, ymax, r);
+  Ux = Ix.ComputeMatrix(X.col(0));
+  Uy = Ix.ComputeMatrix(X.col(1));
+  Vx = Iy.ComputeMatrix(Y.col(0));
+  Vy = Iy.ComputeMatrix(Y.col(1));
+ 
+  for (int i=0; i<r; i++)
+    for (int j=0; j<r; j++) 
+      for (int k=0; k<n; k++) {
+        U(k, i*r+j) = Ux(k, i) * Uy(k, j);
+        V(k, i*r+j) = Vx(k, i) * Vy(k, j);
+      }
+
+  int r2 = r*r;
+  Matrix chebX(r2, 2);
+  Matrix chebY(r2, 2);
+  Vector chebX_1d = Ix.GetChebyshev();
+  Vector chebY_1d = Iy.GetChebyshev();
+  for (int i=0; i<r; i++) 
+    for (int j=0; j<r; j++) {
+      chebX.row(i*r+j) << chebX_1d(i), chebX_1d(j);
+      chebY.row(i*r+j) << chebY_1d(i), chebY_1d(j);
+    }
+
+  for (int i=0; i<r2; i++)
+    for (int j=0; j<r2; j++)
+      S(i, j) = K( chebX.row(i), chebY.row(j) );
+}
+
 int main(int argc, char *argv[]) {
 
-  int n = 10;
-  int r = 3;
+  int n = 5;
+  int r = 2;
 
   for (int i=1; i<argc; i++) {
+    if (!strcmp(argv[i], "-n"))
+      n = atoi(argv[++i]);
     if (!strcmp(argv[i], "-r"))
       r = atoi(argv[++i]);
   }
+  std::cout<<"Number of points: "<<n<<std::endl
+           <<"Chebyshev points (each dimension): "<<r
+	   <<std::endl;
 
-  // random points
-  Vector X = Vector::Random(n); // [-1, 1]
-  Vector Y = Vector::Random(n) + Vector::Constant(n, 10); // [9, 11] 
+  // random 2D points
+  Matrix X = Matrix::Random(n, 2); // [-1, 1]^2
+  Matrix Y = Matrix::Random(n, 2) + Matrix::Constant(n, 2, 10); // [9, 11]^2
 
   Kernel K;
   Matrix A(n, n);
   for (int i=0; i<n; i++) {
     for (int j=0; j<n; j++) {
-      A(i, j) = K( X(i), Y(j) );
+      A(i, j) = K( X.row(i), Y.row(j) );
     }
   }
 
-  Matrix U(n, r), S(r, r), V(n, r);
+  int r2 = r*r;
+  Matrix U(n, r2), S(r2, r2), V(n, r2);
   Compute_lowrank(K, X, -1, 1, Y, 9, 11, r, U, S, V);
-  
+
   Matrix E = A - U*S*V.transpose();
 
+#if 0
   std::cout<<"Hello world!"<<std::endl;
-  std::cout<<"X: "<<X.transpose()<<std::endl;
-  std::cout<<"Y: "<<Y.transpose()<<std::endl;
+  std::cout<<"X: "<<X<<std::endl;
+  std::cout<<"Y: "<<Y<<std::endl;
   std::cout<<"Matrix: \n"<<A<<std::endl;
+#endif
+
   std::cout<<"Error of lowrank: \n"<<E.norm()<<std::endl;
   return 0;
 }
